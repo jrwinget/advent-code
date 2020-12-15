69513cb1352dbb5064144630dfaa95765c3db63e
jrwinget <jwinget@luc.edu>
2020-12-15 15:16:46 -0600

    Add Day 2 solution

---
+++ R/day02.R
@@ -0,0 +1,20 @@
+library(tidyverse)
+
+# read data
+passwords <- read_csv("data/day02.txt", col_names = "policy") %>% 
+  extract(policy, c("min", "max", "letter", "password"), 
+          "(\\d+)-(\\d+) (.): *(.*)", convert = TRUE)
+
+# part 1 solution
+passwords %>% 
+  mutate(letter_count = map2_int(password, letter, str_count)) %>% 
+  filter(letter_count >= min, letter_count <= max) %>% 
+  nrow()
+
+# part 2 solution
+passwords %>% 
+  mutate(letter_count = (str_sub(password, min, min) == letter) +
+           (str_sub(password, max, max) == letter)
+  ) %>% 
+  filter(letter_count == 1) %>% 
+  nrow()
