8470c3d21350154121c62c2d6911364a588b8661
jrwinget <jwinget@luc.edu>
2020-12-15 14:50:58 -0600

    Add *completed* Day 1 solution

--- R/day01.R
+++ R/day01.R
@@ -4,4 +4,8 @@ library(tidyverse)
 raw <- read_delim("data/day01.txt", delim = "\n", col_names = FALSE) %>% 
   rename(report = X1)
 
+# part 1 solution
 prod(intersect(raw$report, 2020 - raw$report))
+
+# part 2 solution
+prod(intersect(raw$report, 2020 - outer(raw$report, raw$report, "+")))
