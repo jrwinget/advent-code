e5ece99460d261efe47300fb5239f12236d5ab1c
jrwinget <jwinget@luc.edu>
2020-12-15 17:58:29 -0600

    Add Day 6 solution

---
+++ R/day06.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+
+# read data
+declarations <- tibble(x = read_lines("data/day06.txt"))
+
+answers <- declarations %>% 
+  mutate(group = cumsum(x == "")) %>% 
+  filter(x != "") %>% 
+  add_count(group, name = "group_total") %>% 
+  separate_rows(x, sep = "") %>% 
+  filter(x != "")
+
+# part 1 solution
+# for each group, count the number of questions to which anyone answered yes
+# what is the sum of those counts?
+answers %>% 
+  distinct(group, x) %>% 
+  nrow()
+
+# part 2 solution
+# for each group, count the number of questions to which everyone answered yes
+# what is the sum of those counts?
+answers %>% 
+  count(group, x, group_total) %>% 
+  filter(n == group_total) %>% 
+  nrow()
