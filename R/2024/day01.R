e07014602d17a18c80cc8188df5a9cff8d6f700a
Jeremy Winget <contact@jrwinget.com>
2024-12-04 11:46:41 -0600

    Add Day 1 part 2 solution

--- R/2024/day01.R
+++ R/2024/day01.R
@@ -8,3 +8,15 @@ input <- read_table("data/2024/day01.txt", col_names = FALSE)
 input |> 
   transmute(diff = abs(sort(X1) - sort(X2))) |> 
   summarize(total_distance = sum(diff))
+
+# part 2 solution
+# what is their similarity score?
+right_counts <- count(input, X2, name = "right_count")
+
+input |>
+  left_join(right_counts, by = c("X1" = "X2")) |>
+  mutate(
+    right_count = coalesce(right_count, 0),
+    similarity = X1 * right_count
+  ) |>
+  summarize(similarity_score = sum(similarity))
