e6947da0227d2c1798904b40732c340fe4e6e663
Jeremy Winget <contact@jrwinget.com>
2024-12-08 18:03:04 -0600

    Add Day 3 solution

---
+++ R/2024/day03.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+
+# read the data
+input <- read_lines("data/2024/day03.txt")
+
+# part 1 solution
+# what is the sum of the results of the multiplications?
+str_extract_all(input, "mul\\(\\d+,\\d+\\)") |>
+unlist() |>
+str_match("mul\\((\\d+),(\\d+)\\)") |>
+as_tibble() |>
+mutate(
+  x = as.numeric(V2),
+  y = as.numeric(V3),
+  product = x * y
+) |>
+summarize(total = sum(product, na.rm = TRUE))
+
+# part 2 solution
+# what is the sum of the results of just the enabled multiplications?
+str_extract_all(input, "do\\(\\)|don't\\(\\)|mul\\(\\d+,\\d+\\)") |>
+  unlist() |>
+  as_tibble() |>
+  mutate(
+    state_change = case_when(
+      value == "do()" ~ TRUE,
+      value == "don't()" ~ FALSE,
+      TRUE ~ NA
+    ),
+    state = accumulate(
+      state_change,
+      ~ ifelse(is.na(.y), .x, .y),
+      .init = TRUE
+    )[-1],
+    x = str_match(value, "mul\\((\\d+),")[, 2] |> as.numeric(),
+    y = str_match(value, ",(\\d+)\\)")[, 2] |> as.numeric(),
+    product = if_else(str_detect(value, "^mul\\(") & state, x * y, 0)
+  ) |>
+  summarize(total = sum(product, na.rm = TRUE))
--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 
 <!-- badges: start -->
 
-![](https://img.shields.io/badge/2024%20star%20count-jrwinget%204*-green.svg)
+![](https://img.shields.io/badge/2024%20star%20count-jrwinget%206*-green.svg)
 <!-- badges: end -->
 
 This repo contains all of my Advent of Code solutions using R. I’m using
@@ -20,4 +20,5 @@ scrapes a user’s Advent of Code star count and displays it as a badge.
 | Day | Puzzle | Solution | Data |
 |:--:|:--:|:--:|:--:|
 | 1 | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [R](R/2024/day01.R) | [Puzzle input](data/2024/day01.txt) |
-| 1 | [Historian Hysteria](https://adventofcode.com/2024/day/2) | [R](R/2024/day02.R) | [Puzzle input](data/2024/day02.txt) |
+| 1 | [Red-Nosed Reports](https://adventofcode.com/2024/day/2) | [R](R/2024/day02.R) | [Puzzle input](data/2024/day02.txt) |
+| 1 | [Mull It Over](https://adventofcode.com/2024/day/3) | [R](R/2024/day03.R) | [Puzzle input](data/2024/day03.txt) |
